5593f1c1ed206bbc4f0fae0e836bcc16d1e84288
bakrifly <bakri.hmouda@gmail.com>
2022-08-23 22:30:06 +0300

    added the color green to the theme

--- src/themes/mainTheme.js
+++ src/themes/mainTheme.js
@@ -1,4 +1,5 @@
 import {createTheme} from "@mui/material";
+import {green} from "@mui/material/colors";
 
 export  const mainTheme = createTheme({
     palette: {
@@ -8,6 +9,11 @@ export  const mainTheme = createTheme({
         },
         secondary: {
             main: '#BA274A'
+        },
+        green:{
+            main: green[500],
+            light: green[300],
+            dark: green[800],
         },
         background: {
             default: '#B2ECE1',
