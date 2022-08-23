9473468f7c0f0bae56f988e3668ba7863db23461
bakrifly <bakri.hmouda@gmail.com>
2022-08-24 00:28:24 +0300

    code cleanup and button functionality

--- src/components/DashBoard/ListItem.js
+++ src/components/DashBoard/ListItem.js
@@ -1,6 +1,17 @@
 import {Button, Card, Grid, Typography,} from "@mui/material";
 
-export default function ListItem() {
+export default function ListItem({confirmDelete, edit}) {
+
+    const id = '123'
+    const data = {
+        costumerName: 'bakri',
+        costumerAddress: 'tripoli',
+        costumerPhoneNumber: '12345678',
+        originAddress: 'China',
+        destinationAddress: 'Lebanon',
+        charge: '3500',
+    }
+
     return <>
 
         <Card sx={{p: 2}}>
@@ -9,44 +20,32 @@ export default function ListItem() {
                 spacing={3}
                 alignItems={'center'}
             >
-                <Grid
-                    item
-                    sm={12}
-                    md={3}
-                >
+                <Grid item sm={12} md={3}>
                     <Typography noWrap>
-                    <strong>Order ID: </strong>    {'id'}
+                        <strong>Order ID: </strong> {'id'}
                     </Typography>
 
                 </Grid>
 
-                <Grid
-                    item
-                    sm={12}
-                    md={4}
-                >
-                    <Typography  noWrap>
-                        <strong>  Customer Name:</strong> {'costumer name'}
-                    </Typography>
-
-                </Grid>
-
-                <Grid
-                    item
-                    sm={12}
-                    md
-                >
+                <Grid item sm={12} md={4}>
                     <Typography noWrap>
-                       <strong>Order Date: </strong> {'1-2-2022'}
+                        <strong> Customer Name:</strong> {'costumer name'}
                     </Typography>
 
                 </Grid>
 
-                <Grid
-                    sx={{display: 'flex', gap: 1}}
-                    item>
-                    <Button variant={'outlined'} size={'small'} color={'green'}>Edit</Button>
-                    <Button variant={'outlined'} size={'small'} color={'error'}>Delete</Button>
+                <Grid item sm={12} md>
+                    <Typography noWrap>
+                        <strong>Order Date: </strong> {'1-2-2022'}
+                    </Typography>
+
+                </Grid>
+
+                <Grid item sx={{display: 'flex', gap: 1}}>
+                    <Button variant={'outlined'} size={'small'} color={'green'}
+                            onClick={() => edit(data)}>Edit</Button>
+                    <Button variant={'outlined'} size={'small'} color={'error'}
+                            onClick={() => confirmDelete(id)}>Delete</Button>
                 </Grid>
 
 
