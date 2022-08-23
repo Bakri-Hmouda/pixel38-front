146f8541f1146894befe1d21c505ac80105dd727
bakrifly <bakri.hmouda@gmail.com>
2022-08-23 15:43:13 +0300

    created an app MUI main theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CssBaseline, ThemeProvider} from "@mui/material";
+import {mainTheme} from "./themes/mainTheme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={mainTheme}>
+            <CssBaseline/>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>
 );
 
 
---
+++ src/themes/mainTheme.js
@@ -0,0 +1,17 @@
+import {createTheme} from "@mui/material";
+
+export  const mainTheme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#2191FB'
+        },
+        secondary: {
+            main: '#BA274A'
+        },
+        background: {
+            default: '#B2ECE1',
+        }
+    }
+
+})
