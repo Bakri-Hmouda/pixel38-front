841234fde7e96a2fde0cda64cfa8e203fc445e93
bakrifly <bakri.hmouda@gmail.com>
2022-08-23 17:19:45 +0300

    added app bar and dashboard routes and empty components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import NavBar from "./components/AppBar/NavBar";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/login'} element={<LoginPage/>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/login'} element={<LoginPage/>}/>
+                <Route path={'/'} element={<NavBar/>}>
+                    <Route path={'/dashboard'} element={<Dashboard/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
